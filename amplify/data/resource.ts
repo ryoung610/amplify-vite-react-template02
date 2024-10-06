1d91b4b4f99dde76b1f71e07e24e8e0433ea021c
ryoung610 <me0610@gmail.com>
2024-10-05 22:32:35 -0400

    Update resource.ts
    update userpool and apikey had dups

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -19,12 +19,12 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: "apiKey",
+    defaultAuthorizationMode: "userPool",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
       expiresInDays: 30,
     },
-        defaultAuthorizationMode: 'userPool',
+        
   },
 });
 
